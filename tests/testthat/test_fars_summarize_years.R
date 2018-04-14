2f1bd4dd06c7ac4046d41c5df523f298e722c256
Jim Tyhurst <jim@tyhurst.com>
2018-04-14 02:54:48 -0700

    Added test cases.

--- tests/testthat/test_fars_summarize_years.R
+++ tests/testthat/test_fars_summarize_years.R
@@ -5,3 +5,17 @@ test_that("valid integer year", {
   expect_equal(dim(testable), c(12, 2))
   expect_equal(names(testable), c("MONTH", "2015"))
 })
+
+test_that("multiple years", {
+  testable <- fars_summarize_years(c(2013, "2015"))
+  expect_equal(dim(testable), c(12, 3))
+  expect_equal(names(testable), c("MONTH", "2013", "2015"))
+})
+
+test_that("unknown year throws exception", {
+  expect_error(fars_summarize_years(c("1905")))
+})
+
+test_that("non-integer year throws exception", {
+  expect_error(fars_summarize_years(c("zuza")))
+})
